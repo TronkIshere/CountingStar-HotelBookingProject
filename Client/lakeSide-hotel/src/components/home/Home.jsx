96efcf0c1bd19898209f9e5df8a6383af2c79602
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-05-14 00:47:51 +0700

    Update for homepage

--- Client/lakeSide-hotel/src/components/home/Home.jsx
+++ Client/lakeSide-hotel/src/components/home/Home.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import MainHeader from '../layout/MainHeader'
 import Parallax from '../common/Parallax'
 import HotelService from '../common/HotelService'
+import RoomCarousel from '../common/RoomCarousel'
 
 const Home = () => {
   return (
     <section>
       <MainHeader/>
-
-      <section>
+      <div className='container'>
+        <RoomCarousel/>
         <Parallax/>
+        <RoomCarousel/>
         <HotelService/>
         <Parallax/>
-      </section>
+        <RoomCarousel/>
+      </div>
     </section>
   )
 }
