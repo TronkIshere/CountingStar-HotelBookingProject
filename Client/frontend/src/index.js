5cdd43de21dfdc7a24f31f0ce60a75eca315d996
Xkckdndnd <duongthithuytrang2402@gmail.com>
2024-07-01 10:59:57 +0700

    Delete folder frontend

--- Client/frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import "./resource/font/fontawesome-free-6.4.2-web/css/all.min.css"
-import "./resource/bootstrap/css/bootstrap.min.css"
-import "./resource/css/base.css"
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './component/App.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
