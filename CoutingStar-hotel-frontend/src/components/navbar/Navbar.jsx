428c042c1d8be98421bef939ada433d9aa544af0
TronkIsHere <nguyenhuutrong11133@gmail.com>
2024-07-07 16:42:22 +0700

    Update Navigate for authentication and authorization.

--- CoutingStar-hotel-frontend/src/components/navbar/Navbar.jsx
+++ CoutingStar-hotel-frontend/src/components/navbar/Navbar.jsx
@@ -2,6 +2,9 @@ import React, { useContext, useState } from "react";
 import "./navbar.css";
 import { Link } from "react-scroll";
 import { AuthContext } from "../utils/AuthProvider";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = ({ onLoginClick, onRegisterClick }) => {
   const [showAccount, setShowAccount] = useState(false);
@@ -14,14 +17,43 @@ const Navbar = ({ onLoginClick, onRegisterClick }) => {
   const isLoggedIn = user !== null;
   const userRole = localStorage.getItem("userRole");
 
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    auth.handleLogout();
+    window.location.reload();
+    navigate("/", { state: { message: " You have been logged out!" } });
+  };
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <span className="logo">CoutingStar</span>
         <div className="navItems">
-
           {isLoggedIn ? (
-            <div>Đã đăng nhập</div>
+            <div className="accountContainer">
+              <button className="userButton" onClick={handleAccountClick}>
+                <FontAwesomeIcon icon={faUserCircle} />
+              </button>
+              {showAccount && (
+                <div className="dropdown">
+                  <ul>
+                    <li>
+                      <button>Người dùng</button>
+                    </li>
+                    {userRole === "ROLE_HOTEL_OWNER" && (
+                      <li>
+                        <button>Quản lý khách sạn</button>
+                      </li>
+                    )}
+                    <li>
+                      <button onClick={handleLogout}>Đăng xuất</button>
+                    </li>
+                  </ul>
+                </div>
+              )}
+            </div>
           ) : (
             <>
               <button className="postHotelButton">Đăng phòng của bạn</button>
